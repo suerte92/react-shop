1dbdc024cd3f14210a8bfa9b755063991dc30d29
Anibal Oziel <anibaloziel.alvarado@gmail.com>
2022-07-14 10:41:32 -0500

    problema solucionado con éxito

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductItem from '@components/ProductItem';
-import useGetProducts from '@hooks/useGetProduct';
+import useGetProducts from '../hooks/useGetProducts';
 import '@styles/ProductList.scss';
 
 
